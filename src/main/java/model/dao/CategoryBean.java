72fc428c6c8adadbabb73d354181644ab1df5c7e
issei-yamashita-cs <issei.yamashita@comsize.com>
2025-08-12 17:53:08 +0900

    2025-08-12yamashita

---
+++ src/main/java/model/dao/CategoryBean.java
@@ -0,0 +1,15 @@
+package model.dao;
+
+public class CategoryBean {
+
+	public void setCategory_name(String string) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+	public void setCategory_id(String string) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+}
---
+++ src/main/java/model/dao/StatusBean.java
@@ -0,0 +1,15 @@
+package model.dao;
+
+public class StatusBean {
+
+	public void setStatus_code(String string) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+	public void setStatus_name(String string) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+}
