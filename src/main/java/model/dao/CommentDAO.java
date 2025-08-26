6402fb0fc7e7dc4b4632062ebd4726d6baa4ea63
issei-yamashita-cs <issei.yamashita@comsize.com>
2025-08-26 09:12:32 +0900

    0826 朝

--- src/main/java/model/dao/CommentDAO.java
+++ src/main/java/model/dao/CommentDAO.java
@@ -11,33 +11,15 @@ import model.entity.CommentBean;
 
 public class CommentDAO {
 
-	public List<CommentBean> selectComment() throws SQLException, ClassNotFoundException {
+	public List<CommentBean> selectComment(int taskId) throws SQLException, ClassNotFoundException {
 		List<CommentBean> commentList = new ArrayList<>();
 
-		String sql = "SELECT\n"
-				+ "    t1.task_id,\n"
-				+ "    t1.task_name,\n"
-				+ "    t4.category_name,\n"
-				+ "    t1.limit_date,\n"
-				+ "    t2.user_name,\n"
-				+ "    t3.status_name,\n"
-				+ "    t1.memo,\n"
-				+ "    t1.create_datetime,\n"
-				+ "    t1.update_datetime,\n"
-				+ "    t5.comment_id,\n"
-				+ "    t5.user_id,\n"
-				+ "    t5.comment,\n"
-				+ "    t5.update_datetime\n"
-				+ "FROM\n"
-				+ "    t_task t1\n"
-				+ "INNER JOIN m_user t2 ON t1.user_id = t2.user_id\n"
-				+ "INNER JOIN m_status t3 ON t1.status_code = t3.status_code\n"
-				+ "INNER JOIN m_category t4 ON t1.category_id = t4.category_id\n"
-				+ "INNER JOIN t_comment t5 ON t1.task_id = t5.task_id";
-
+		String sql = "SELECT * FROM t_comment WHERE task_id = ?";
+		
 		try (Connection con = ConnectionManager.getConnection();
 				PreparedStatement pstmt = con.prepareStatement(sql)) {
 
+			pstmt.setInt(1, taskId);
 			ResultSet res = pstmt.executeQuery();
 
 			while (res.next()) {
@@ -67,6 +49,34 @@ public class CommentDAO {
 				pstmt.setInt(1, itemInfo.getTask_id());
 				pstmt.setString(2, itemInfo.getUser_id());
 				pstmt.setString(3, itemInfo.getComment());
+
+				processingNumber = pstmt.executeUpdate();
+			}
+		
+			return processingNumber;
+		}
+		
+		public int deleteComment(CommentBean dCom) throws SQLException, ClassNotFoundException {
+			int processingNumber = 0;
+			String sql = "DELETE FROM t_comment WHERE comment_id = ?";
+			try (Connection con = ConnectionManager.getConnection();
+					PreparedStatement pstmt = con.prepareStatement(sql)) {
+
+				pstmt.setInt(1, dCom.getComment_id());
+
+				processingNumber = pstmt.executeUpdate();
+			}
+		
+			return processingNumber;
+		}
+		
+		public int deleteComment(CommentBean dCom) throws SQLException, ClassNotFoundException {
+			int processingNumber = 0;
+			String sql = "DELETE FROM t_comment WHERE comment_id = ?";
+			try (Connection con = ConnectionManager.getConnection();
+					PreparedStatement pstmt = con.prepareStatement(sql)) {
+
+				pstmt.setInt(1, dCom.getComment_id());
 
 				processingNumber = pstmt.executeUpdate();
 			}
